cc41b002d2d27717790e6ad6cd94459163b50412
Maksim Bazanov <bazanov1996@mail.ru>
2022-06-10 17:24:00 +0300

    initial commit

---
+++ src/main/java/com/testtask/gismeteoapp/GisMeteoAppApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.gismeteoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisMeteoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GisMeteoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testtask/gismeteoapp/GisMeteoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.gismeteoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisMeteoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
